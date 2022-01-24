2a82b6aa529ccbaa21f18bcd3cc9bed5226b1749
UserName1080Alyner <76223883+UserName1080Alyner@users.noreply.github.com>
2022-01-24 16:09:19 -0300

    Delete start.bat

--- start.bat
+++
@@ -1,5 +0,0 @@
-java -Xmx1024M -Xms1024M -jar spigot-1.8.8-R0.1-SNAPSHOT-latest.jar
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
-unzip /path/to/ngrok.zip
-./ngrok authtoken 1ywyJyfNMhuuN2RX2bsXetCHBsX_6FAsDPHWNMenoXZ9p9mfN
-./ngrok tcp 25565
